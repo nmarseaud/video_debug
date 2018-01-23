146178b17c1d31bc1005a57d5d164da8cd795edf
nmarseaud <nicolas.marseaud@outlook.fr>
2018-01-23 17:30:13 +0100

    Installation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  video;
+  constructor(public navCtrl: NavController, private mediaCapture: MediaCapture) {
+      let options: CaptureVideoOptions = { limit: 1, duration:180, };
+      this.mediaCapture.captureVideo(options)
+      .then(
+        (videodata: MediaFile[]) => {
+            console.log(videodata);
+            this.video = { url : videodata[0].localURL };
+            videodata[0].getFormatData(function(formatData) {
+                console.log(formatData);
+            });
+        },
+        (err: CaptureError) => {
+            console.error(err)
+        }
+      );
+  }
 
+  videoCapture() {
+      console.log("Video Capture");
+      let options: CaptureVideoOptions = { limit: 1, duration:180, };
+      this.mediaCapture.captureVideo(options)
+      .then(
+        (videodata: MediaFile[]) => {
+            console.log(videodata);
+            this.video = { url : videodata[0].fullPath };
+            videodata[0].getFormatData(function(formatData) {
+                console.log(formatData);
+            });
+        },
+        (err: CaptureError) => {
+            console.error(err)
+        }
+      );
   }
 
 }
